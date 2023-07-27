de05f826fa83b3dd6cd65ae13c8286fd8d2cb861
chethan25 <chethan25.bpac@gmail.com>
2023-07-27 18:02:29 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const modal = document.getElementById('modal');
+const loginBtn = document.getElementById('login-btn');
+const closeBtn = document.getElementById('close');
+
+loginBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
